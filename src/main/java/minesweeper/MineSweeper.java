b0c5e1cc7716ce7a4cde3c78778e9f37bb079c50
Valentyn Serediuk <serediukit@gmail.com>
2023-08-05 23:53:00 +0300

    add capability to draw images

--- src/main/java/minesweeper/MineSweeper.java
+++ src/main/java/minesweeper/MineSweeper.java
@@ -5,21 +5,30 @@ import java.awt.*;
 
 public class MineSweeper extends JFrame {
     private JPanel gamingPanel;
-    private final int COLS = 40;
-    private final int ROWS = 25;
-    private final int IMAGE_SIZE = 30;
+    private final int COLS = 30;
+    private final int ROWS = 15;
+    private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
         new MineSweeper();
     }
 
     private MineSweeper() {
+        setImages();
         initPanel();
         initFrame();
     }
 
     private void initPanel() {
-        gamingPanel = new JPanel();
+        gamingPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box : Box.values()) {
+                    g.drawImage((Image) box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                }
+            }
+        };
         gamingPanel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
         add(gamingPanel);
     }
@@ -32,4 +41,16 @@ public class MineSweeper extends JFrame {
         setResizable(false);
         setVisible(true);
     };
+
+    private void setImages() {
+        for (Box box : Box.values()) {
+            box.image = getImage(box.name());
+        }
+    }
+
+    private Image getImage(String name) {
+        String filename = "res/img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon(filename);
+        return icon.getImage();
+    }
 }
